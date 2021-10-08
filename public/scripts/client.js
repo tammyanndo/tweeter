9384b5ba7ca07fb3a09874c046a4a60ba5989bf8
Tammy <tammyanndo@gmail.com>
2021-10-07 21:39:40 -0400

    added alerts for empty text and over character text

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,31 @@
  */
 
 $(document).ready(function () {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
   const createTweetElement = tweet => {
     const newTweet =
@@ -64,5 +64,36 @@ $(document).ready(function () {
     };
   };
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET'}) 
+    .then(function(response) {
+      renderTweets(response)
+      console.log('Success:', response )
+    })
+  }
+
+  $( ".input-form" ).submit(function( event ) {
+    event.preventDefault();
+    console.log('i am in submit tweet')
+
+    let data = $(this).serialize();
+    console.log("this is the serialized data:", data)
+
+    let dataLength = data.length;
+    const overBy = 140 - dataLength
+
+    if (dataLength > 140) {
+      return alert(`You have exceeded the 140 character limit by ${overBy} characters.`)
+    } 
+    if (dataLength === 0 ) {
+      return alert('Your tweet is blank.')
+    }
+
+    $.post('/tweets', data, function() {
+      console.log('this is the post request')
+      $('#tweets-container').empty()
+      loadTweets()
+      $('textarea').val("");
+    })
+    })
 });
